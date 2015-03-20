be33e4d2bb330da682eb958fcff1f60e1139ff29
Craig Neuwirt <cneuwirt@gmail.com>
2015-03-20 07:25:47 -0500

    Created mytodo scaffold

---
+++ demo/mytodo/app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
