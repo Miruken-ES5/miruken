29dfade6efcedc44afd691e65e6b764fa7ac5ba3
Craig Neuwirt <cneuwirt@gmail.com>
2015-03-22 15:42:13 -0500

    Use feature with context in demo

--- demo/mytodo/app/scripts/controllers/todo.js
+++ demo/mytodo/app/scripts/controllers/todo.js
@@ -1,13 +1,27 @@
-eval(miruken.mvc.namespace);
+new function () {
 
-mytodoApp.export(
-    Controller.extend({
-        $inject: ['$route', '$location'],
-        constructor: function ($route, $location) {
-            this.extend({
-                todos: [$location.path(), $route.current.controller, $route.current.templateUrl ]
-            });
+    var todo = new base2.Package(this, {
+        name:    "todo",
+        parent:  mytodoApp,
+        imports: "miruken,miruken.mvc",
+        exports: "TodoController"
+    });
+
+    eval(this.imports);
+
+    var TodoFeature = Protocol.extend({
+        fetchTodos: function () {}
+    });
+
+    var TodoController =  Controller.extend({
+        getTodos: function () {
+            return TodoFeature(this.context).fetchTodos();
+        },
+
+        fetchTodos: function () {
+            return [ 'Item 1', 'Item 2', 'Item 3' ];
         }
-    }),
-    "TodoController");
+    });
 
+    eval(this.exports);
+}
