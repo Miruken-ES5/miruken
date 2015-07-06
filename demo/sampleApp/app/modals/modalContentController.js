7391e2d221240baedc1e77d7be2f714410b81de3
Michael Dudley <michael.dudley@improvingenterprises.com>
2015-07-06 08:25:25 -0500

    renaming the modal files

---
+++ demo/sampleApp/app/modals/modalContentController.js
@@ -0,0 +1,24 @@
+﻿new function() {
+
+    var sampleApp = new base2.Package(this, {
+        name:    "sampleApp",
+        imports: "miruken,miruken.mvc",
+        exports: "ModalContentController"
+    });
+
+    eval(this.imports);
+
+    var disposed = 0;
+    
+    var ModalContentController = Controller.extend(Disposing, {
+        dispose: function () { ++disposed; },
+        message: 'Hello',
+        items: [1,2,3],
+        getDisposed: function(){
+            return disposed;
+        }
+    });
+
+    eval(this.exports);
+
+}
