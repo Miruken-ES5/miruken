69f31ec95e7d5343da8d0f7b26094e9d7b62392b
Michael Dudley <michael.dudley@improvingenterprises.com>
2016-06-26 18:32:04 -0500

    Added mixins example

---
+++ demo/sampleCode/tests/mixinsTests.js
@@ -0,0 +1,18 @@
+new function(){
+
+	eval(base2.mixins.namespace)
+
+	describe("statics", () => {
+		
+		var rover = new Animal('Rover');
+
+		it("should export Animal", ()=>{
+			Animal.should.not.be.null;
+		});
+
+		it("should have a verify method", () => {
+			rover.verify().should.be.true
+		});
+
+	});
+}
