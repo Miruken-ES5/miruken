76a8e6692aa1f3ddc8af47f3c6625b708548fae5
mjesse <michael.jesse@improving.com>
2016-06-28 18:01:47 -0500

    SampleCode changes * change src files * change test files

--- demo/sampleCode/tests/instancesTests.js
+++ demo/sampleCode/tests/instancesTests.js
@@ -1,11 +1,51 @@
-new function(){
-	
-	eval(base2.instances.namespace)
+/* 
+ * The MIT License
+ *
+ * Copyright 2016 Craig Neuwirt, Michael Dudley.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-	describe("instances", () => {
-		it("should export a Ticket", () => {
-			Ticket.should.not.be.nothing;
-		});
-	});
+new function(){
+
+  eval(base2.instances.namespace)
+
+  describe("instances", () => {
+    it("should export a Logger", () => {
+      Logger.should.not.be.nothing;
+    });
+
+    describe("alert logger instance", () => {
+      it("should now have alert capability", () => {
+        let myLogger = new Logger("console");
+        let alertLogger = new Logger("alert");
+
+        alertLogger.extend({
+          alert: function(message) {
+            alert(message);
+          }
+        });
+
+        myLogger.hasOwnProperty("alert").should.be.false;
+        alertLogger.hasOwnProperty("alert").should.be.true;
+      });
+    });
+  });
 
 }
